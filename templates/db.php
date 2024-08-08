5f7239e5a601b4ad3449e32768dec22bfc399f90
tarosjk <tarosjk@gmail.com>
2024-08-08 12:02:51 +0900

    DB接続一元化

---
+++ templates/db.php
@@ -0,0 +1,13 @@
+<?php
+try {
+  $dsn = 'mysql:host=localhost;dbname=pizza_interplan;charset=utf8';
+  $user = 'pizzataro';
+  $pass = 'Q62W.Khl5b)4]8-k';
+  $option = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+  ];
+  $db = new PDO($dsn, $user, $pass, $option);
+} catch (PDOException $e) {
+  var_dump($e->errorInfo);
+}
