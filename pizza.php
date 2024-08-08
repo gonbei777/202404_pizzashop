7fca955725a2df720ceae9f6ad6c7e548f03387e
tarosjk <tarosjk@gmail.com>
2024-08-08 10:22:06 +0900

    PDOの設定

--- pizza.php
+++ pizza.php
@@ -1,4 +1,19 @@
 <?php
+
+try {
+  $dsn = 'mysql:host=localhost;dbname=pizza_interplan;charset=utf8';
+  $user = 'pizzataro';
+  $pass = 'Q62W.Khl5b)4]8-k';
+  $option = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+  ];
+  $db = new PDO($dsn, $user, $pass, $option);
+} catch (PDOException $e) {
+  var_dump($e->errorInfo);
+}
+
+
 require './templates/header.php';
 // include './templates/header.php';
 ?>
